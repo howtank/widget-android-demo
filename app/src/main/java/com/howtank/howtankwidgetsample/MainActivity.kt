2f69d0e7c3c86375422a55ad7b18301ffa636c98
quanle <phatgialt@gmail.com>
2023-03-30 22:10:15 +0700

    upgraded android API target to 33

--- app/src/main/java/com/howtank/howtankwidgetsample/MainActivity.kt
+++ app/src/main/java/com/howtank/howtankwidgetsample/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.howtank.howtankwidgetsample
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.howtank.widget.data.HTPurchaseParameters
 import com.howtank.widget.main.HowtankWidget
 import com.howtank.widget.main.HowtankWidgetEvent
 import com.howtank.widget.main.HowtankWidgetHandler
 import kotlinx.android.synthetic.main.activity_main.*
-import android.text.method.ScrollingMovementMethod
-import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,37 +30,37 @@ class MainActivity : AppCompatActivity() {
 
         /// Inits the widget. This has to be called before "browse".
         HowtankWidget.getInstance()
-                .setHandler(object : HowtankWidgetHandler {
-                    override fun onLinkSelected(link: String?) {
-                        if (link != null) {
-                            appendText("Link selected $link")
+            .setHandler(object : HowtankWidgetHandler {
+                override fun onLinkSelected(link: String?) {
+                    if (link != null) {
+                        appendText("Link selected $link")
+                    }
+                }
+
+                override fun widgetEvent(event: HowtankWidgetEvent?) {
+                    when (event) {
+                        HowtankWidgetEvent.INITIALIZED -> {
+                            appendText("Widget is initialized")
+                            moreActionsView.visibility = View.VISIBLE
                         }
+                        HowtankWidgetEvent.OPENED -> appendText("Widget is opened")
+                        HowtankWidgetEvent.DISABLED -> appendText("Widget disabled")
+                        HowtankWidgetEvent.MOVED -> appendText("Widget buddle moved")
+                        HowtankWidgetEvent.DISPLAYED -> appendText("Widget is displayed")
+                        HowtankWidgetEvent.HIDDEN -> appendText("Widget is hidden")
+                        else -> {}
                     }
+                }
 
-                    override fun widgetEvent(event: HowtankWidgetEvent?) {
-                        when (event) {
-                            HowtankWidgetEvent.INITIALIZED -> {
-                                appendText("Widget is initialized")
-                                moreActionsView.visibility = View.VISIBLE
-                            }
-                            HowtankWidgetEvent.OPENED -> appendText("Widget is opened")
-                            HowtankWidgetEvent.DISABLED -> appendText("Widget disabled")
-                            HowtankWidgetEvent.MOVED -> appendText("Widget buddle moved")
-                            HowtankWidgetEvent.DISPLAYED -> appendText("Widget is displayed")
-                            HowtankWidgetEvent.HIDDEN -> appendText("Widget is hidden")
-                            else -> {}
-                        }
-                    }
+                override fun widgetUnavailable(reason: String?) {
+                    appendText("Howtank widget is not available: $reason")
+                }
 
-                    override fun widgetUnavailable(reason: String?) {
-                        appendText("Howtank widget is not available: $reason")
-                    }
-
-                })
-                .setVerboseOn(verbose)
-                .useSandboxPlatform()
-                //.setCustomImages(INACTIVE_IMAGE_RESOURCE, ACTIVE_IMAGE_RESOURCE) // custom bubble images
-                .init(this, hostId)
+            })
+            .setVerboseOn(verbose)
+            .useSandboxPlatform()
+            //.setCustomImages(INACTIVE_IMAGE_RESOURCE, ACTIVE_IMAGE_RESOURCE) // custom bubble images
+            .init(this, hostId)
 
         actionsTextView.text = ""
         actionsTextView.movementMethod = ScrollingMovementMethod()
@@ -73,17 +73,25 @@ class MainActivity : AppCompatActivity() {
     private fun initButtons() {
         showDefaultPositionButton.setOnClickListener {
             // Shows the widget at the default position. Typically, this method is called on each activity "onResume"
-            HowtankWidget.getInstance().browse(this, true, "Page with default position", "http://c1.howtank.com/default")
+            HowtankWidget.getInstance()
+                .browse(this, true, "Page with default position", "http://c1.howtank.com/default")
         }
 
         showCustomPositionButton.setOnClickListener {
             // Shows the widget at a custom position. Typically, this method is called on each activity "onResume"
-            HowtankWidget.getInstance().browse(this, true, "Page with custom position", "http://c1.howtank.com/custom", "10 10 - -")
+            HowtankWidget.getInstance().browse(
+                this,
+                true,
+                "Page with custom position",
+                "http://c1.howtank.com/custom",
+                "10 10 - -"
+            )
         }
 
         hideButton.setOnClickListener {
             /// Hides the widget. Typically, this method is called on a new activity "onResume"
-            HowtankWidget.getInstance().browse(this, false, "Page with hidden widget", "http://c1.howtank.com/hidden")
+            HowtankWidget.getInstance()
+                .browse(this, false, "Page with hidden widget", "http://c1.howtank.com/hidden")
         }
 
         openButton.setOnClickListener {
@@ -116,7 +124,8 @@ class MainActivity : AppCompatActivity() {
         val newText = actionsTextView.text.toString() + "- " + text + "\n"
         this.actionsTextView.text = newText
         if (actionsTextView.layout != null) {
-            val scrollAmount = actionsTextView.layout.getLineTop(actionsTextView.lineCount) - actionsTextView.height
+            val scrollAmount =
+                actionsTextView.layout.getLineTop(actionsTextView.lineCount) - actionsTextView.height
             if (scrollAmount > 0) {
                 actionsTextView.scrollTo(0, scrollAmount)
             } else {
